4c511cfb04507b1d1f95ecfce9442909a4595681
沈冠翔 <nkheros@126.com>
2023-08-16 15:27:08 +0800

    change multithread header

--- include/Multithread.h
+++ include/Multithread.h
@@ -2,24 +2,25 @@
 #define __MULTITHREAD_H__
 #include "LoopSimplify.h"
 
-struct LoopInfo{
+struct LoopInfo
+{
     Induction indvar;
-    bool right_end_inclusive = false;
-    bool lt = true;
-    std::pair<Operand*,Operand*>indvar_range; //[first,second)
-    std::vector<BasicBlock*> loop_blocks;
-    BasicBlock* loop_header;
-    std::vector<BasicBlock*> loop_exiting_blocks;
+    int cmpType;                                  // eg CmpInstruction::GE
+    std::pair<Operand *, Operand *> indvar_range; //[first,second)
+    std::vector<BasicBlock *> loop_blocks;
+    BasicBlock *loop_header;
+    std::vector<BasicBlock *> loop_exiting_blocks;
 };
 class Multithread
 {
     int nr_threads = 4;
     LoopInfo loop;
     void insert_opt_jump();
+
 public:
     Multithread(); // auto find simple loop
-    Multithread(LoopInfo loop_info):loop(loop_info){}
-    void transform(); 
+    Multithread(LoopInfo loop_info) : loop(loop_info) {}
+    void transform();
     void checkForm();
 };
 #endif // __MULTITHREAD_H__
